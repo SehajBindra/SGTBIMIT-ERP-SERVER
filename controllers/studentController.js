736b9a6cadb965c1152eebea5474b8b5aeb3b3a7
Utkp5 <utkp200@gmail.com>
2023-03-24 13:59:51 +0530

    studentcntrl chng

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,7 +1,56 @@
-const Student = require('../models/student');
+const studentModel = require('../models/student');
+const {comparePassword, hashPassword} = require("../middleware/authpassword");
 
 const studentSignupController = async(req,res) => {
+    try {
+        
+        const {firstName,lastName,Email,password,phone,address} = req.body;
+        
+        if (!firstName && !lastName && !Email && !password && !phone && !address) {
+            return res.status(401).send({message : "All fields are required"});
+        }
+        if (firstName === lastName) {
+            return res.status(401).send({message : "firstName and lastName should not be the same"});
+        }
+        if (phone.length > 10 || phone.length < 10) {
+            return res.status(400).send({ message: "You have typed wrong phone number" });
+        }
+        if (password.length < 8) {
+            return res.status(401).send({message : "password must be of min 8 characters"});
+        }
 
+        const oldUser = await studentModel.findOne({Email});
+
+        if (oldUser) {
+            return res.status(401).send({message : "You are already registered user kindly login"});
+        }
+
+        const hashedpassword = await hashPassword(password);
+
+        const user = await new studentModel({
+            firstName,
+            lastName,
+            Email,
+            password: hashedpassword,
+            phone,
+            address,
+        }).save();
+
+        return res.status(200).send({
+            success: true,
+            message : "User Register Successfully",
+            user, 
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            message : 'Error in Registration',
+            error,
+        });
+    }
 }
 
 
