60430c60c3479570055d200dea2ee04d883ad290
Utkp5 <utkp200@gmail.com>
2023-03-24 12:04:42 +0530

    index.js pckjson  chng

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 
@@ -8,6 +9,18 @@ const PORT = process.env.PORT || 5000;
 app.get("/", function(req,res) {
     return res.status(200).send("Everthing is working fine --");
 })
+
+
+//env 
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+app.use(express.json());
+app.use(cors({origin: true, credentials: true}));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
 
 
 app.listen(PORT, function (error) {
