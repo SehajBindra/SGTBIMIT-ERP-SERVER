9ab4fc5870cd88023ebc52426555c0af3f2bfd61
Utkp5 <utkp200@gmail.com>
2023-03-24 18:44:54 +0530

    studentcntrl studentroute chng

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {studentSignupController} = require("../controllers/studentController");
+const {studentSignupController, StudentSigninController} = require("../controllers/studentController");
 
 
-
+// register
 router.post("/Signup", studentSignupController);
 
+//login
+router.post("/Signin", StudentSigninController)
 
 
 module.exports = router;
