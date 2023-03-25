9dd8af715c1e89111c9c02b91c45a782f35befe3
Utkp5 <utkp200@gmail.com>
2023-03-25 12:36:31 +0530

    authmiddlew chng

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,8 +1,31 @@
 const studentModel = require("../models/student");
+const facultyModel = require("../models/faculty");
 
 const isAdmin = async (req, res, next) => {
     try {
-      const user = await studentModel.findById(req.user._id);
+      const user = await studentModel.findById(req.body._id);
+      if (user.role !== 2) {
+        return res.status(401).send({
+          success: false,
+          message: "UnAuthorized Access",
+        });
+      } else {
+        next();
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(401).send({
+        success: false,
+        error,
+        message: "Error in admin middelware",
+      });
+    }
+  };
+
+
+const isFaculty = async (req, res, next) => {
+    try {
+      const user = await facultyModel.findById(req.body._id);
       if (user.role !== 1) {
         return res.status(401).send({
           success: false,
@@ -22,4 +45,4 @@ const isAdmin = async (req, res, next) => {
   };
 
 
-  module.exports = isAdmin;
+  module.exports = isAdmin, isFaculty;
